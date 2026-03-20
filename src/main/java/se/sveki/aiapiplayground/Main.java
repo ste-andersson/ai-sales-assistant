54c45e80824791711b78999e6ce1d6fc3ea34abb
Stefan Andersson <stefan.andersson@sveki.se>
2026-03-20 12:17:44 +0100

    Refactor Main to remove unnecessary imports

--- src/main/java/se/sveki/aiapiplayground/Main.java
+++ src/main/java/se/sveki/aiapiplayground/Main.java
@@ -1,12 +1,5 @@
 package se.sveki.aiapiplayground;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
-import se.sveki.aiapiplayground.utilities.PromptLoader;
-
-import java.time.LocalDate;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) throws Exception {
 
